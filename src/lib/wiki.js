468834a8359b18c3cc62b8cd6b1a7815591bb11c
pyro <pyrolyzed@proton.me>
2025-11-24 15:39:08 -0600

    Fixed it

--- src/lib/wiki.js
+++ src/lib/wiki.js
@@ -4,42 +4,42 @@ const https = require("https");
 const WIKI_URL = "https://www.poewiki.net/w/api.php";
 
 
-module.exports = {
-    get(url) {
-        return new Promise((resolve, reject) => {
-            let data = "";
+const get = (url) => {
+    return new Promise((resolve, reject) => {
+        let data = "";
 
-            https.get(url, response => {
-                response.on("data", chunk => {
-                    data += chunk;
-                });
+        https.get(url, response => {
+            response.on("data", chunk => {
+                data += chunk;
+            });
 
-                response.on("end", () => {
-                    resolve(data);
-                });
+            response.on("end", () => {
+                resolve(data);
+            });
 
-                response.on("error", err => {
-                    reject(err);
-                });
-            }).on("error", err => {
+            response.on("error", err => {
                 reject(err);
             });
+        }).on("error", err => {
+            reject(err);
         });
-    },
+    });
+}
 
-    async getPage(page) {
-        const query = new URLSearchParams({
-            action: "opensearch",
-            format: "json",
-            search: page,
-        });
-        const fullUrl = `${WIKI_URL}?${query.toString()}`;
+const getPage = async (page) => {
+    const query = new URLSearchParams({
+        action: "opensearch",
+        format: "json",
+        search: page,
+    });
+    const fullUrl = `${WIKI_URL}?${query.toString()}`;
 
-        const rawData = await get(fullUrl);
-        const data = JSON.parse(rawData);
-        const matchesIndex = 3;
+    const rawData = await get(fullUrl);
+    const data = JSON.parse(rawData);
+    const matchesIndex = 3;
 
-        const matches = data[matchesIndex];
-        return matches.length > 0 ? matches[0] : null;
-    }
-};
+    const matches = data[matchesIndex];
+    return matches.length > 0 ? matches[0] : null;
+}
+
+module.exports = { get, getPage }
--- src/main.js
+++ src/main.js
@@ -1,28 +1,26 @@
-//const path = require("path")
-//const fs = require("fs")
+const fs = require("fs");
+const path = require("path");
 const { Client, Events, GatewayIntentBits } = require("discord.js");
 const { token } = require("../config.json");
 const { getPage } = require("./lib/wiki");
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
+const messageHandlers = new Map();
 
-//const messageHandlers = new Map();
+const messagesPath = path.join(__dirname, "messages");
+const messageFiles = fs.readdirSync(messagesPath).filter(file => file.endsWith(".js"));
 
-//const messagesPath = path.join(__dirname, "messages");
-//const messageFiles = fs.readdirSync(messagesPath).filter(file => file.endsWith(".js"));
+for (const file of messageFiles) {
+    const filePath = path.join(messagesPath, file);
+    const messageModule = require(filePath);
 
-//for (const file of messageFiles) {
-//    const filePath = path.join(messagesPath, file);
-//    const messageModule = require(filePath);
-//
-//    if (
-//        typeof messageModule.name === "string" &&
-//        typeof messageModule.execute === "function"
-//    ) {
-//        messageHandlers.set(messageModule.name.toLowerCase(), messageModule);
-//    }
-//}
-
+    if (
+        typeof messageModule.name === "string" &&
+        typeof messageModule.execute === "function"
+    ) {
+        messageHandlers.set(messageModule.name.toLowerCase(), messageModule);
+    }
+}
 
 client.once(Events.ClientReady, client => {
     console.log(`Logged in as ${client.user.tag}`);
@@ -31,15 +29,14 @@ client.once(Events.ClientReady, client => {
 client.on("messageCreate", async (message) => {
     if (message.author.bot) return false;
 
-
-    //const handler = messageHandlers.get(message.toString().toLowerCase())
-    //if (handler) {
-    //   handler.execute(client, message)
-    //}
-
-
     // Matches double brackets [[string]] and maps to the the value(s) inside
     const matches = [...message.toString().matchAll(/\[\[([^[\]]+?)\]\]/g)].map(m => m[1]);
+
+
+    const handler = messageHandlers.get(message.toString().toLowerCase())
+    if (handler) {
+        handler.execute(client, message)
+    }
 
     matches.forEach(async match => {
         const page = await getPage(match);
