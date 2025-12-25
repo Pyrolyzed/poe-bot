1e38f6cfdee18555e7a5df2dedb5620f97057bbe
pyro <pyrolyzed@proton.me>
2025-12-25 15:36:59 -0600

    Added help command

--- src/messages/leveling_items.js
+++ src/messages/leveling_items.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: "!!Leveling Items",
+    name: "!!Leveling",
     execute(client, message) {
         message.reply("Great resource for leveling items: [APeanutButterZebraHacker's Comprehensive Guide for Leveling Uniques](https://old.reddit.com/r/pathofexile/comments/1l3c09v/a_peanutbutterzebrahacker_comprehensive_guide_for/)")
     }
