46a528f96bcd61ad9f9a89b91c4c763f180be4cd
Pyrolyzed <pyrolyzed@proton.me>
2026-01-12 18:33:31 -0600

    maybe fixed

--- src/lib/random.js
+++ src/lib/random.js
@@ -1,7 +1,8 @@
 const getRandomValue = (array) => {
     const length = array.length;
     const random = Math.floor(Math.random(length));
-
+    console.log(length)
+    console.log(random)
     return array[random];
 };
 
