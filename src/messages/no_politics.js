1dcf77ccc4cefbf79a127c166eec53b4a3809609
Pyrolyzed <pyrolyzed@proton.me>
2026-03-13 18:07:13 -0500

    feat: Changed rtfr command reply

--- src/messages/no_politics.js
+++ src/messages/no_politics.js
@@ -1,6 +1,11 @@
 module.exports = {
     name: "!!RTFR",
-    execute(client, message) {
-        message.reply("Read the fucking rules: https://discord.com/channels/637184660496973834/719875122583961662");
+    async execute(client, message) {
+        if (message.reference) {
+            const referenceMessage = await message.fetchReference()
+            referenceMessage.reply(`<@${referenceMessage.author.id}> Read the fucking rules: https://discord.com/channels/637184660496973834/719875122583961662`);
+        } else {
+            message.reply("Read the fucking rules: https://discord.com/channels/637184660496973834/719875122583961662")
+        }
     }
 }
