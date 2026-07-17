6f53d941254e43afd57513f1ffd18a888ff754a0
Pyrolyzed <pyrolyzed@proton.me>
2026-07-17 13:28:48 -0500

    I'm dumb

--- src/commands/poe/stashsale.js
+++ src/commands/poe/stashsale.js
@@ -3,21 +3,6 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 const url = "https://poedb.tw/us/";
-function getCountdown(unixTimestamp) {
-    const targetDate = new Date(parseInt(unixTimestamp) * 1000);
-    const now = new Date();
-
-    const diff = targetDate - now;
-
-    if (diff <= 0) return "Event started";
-
-    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
-
-    return `${days}d ${hours}h ${minutes}m ${seconds}s`;
-}
 module.exports = {
     data: new SlashCommandBuilder().setName("stashtabsale").setDescription("Get the date of the next stash tab sale."),
     async execute(interaction) {
@@ -30,7 +15,7 @@ module.exports = {
         const body = $("h5.card-header")
             .filter((_, el) => $(el).text().includes("Stash Tab"))
             .siblings(".card-body");
-        const timestamp = getCountdown(body.find("[data-displaytime]").attr("data-displaytime"));
-        await interaction.reply("The next stash tab sale is in " + timestamp);
+        const timestamp = body.find("[data-displaytime]").attr("data-displaytime");
+        await interaction.reply(`The next stash tab sale is at <t:${timestamp}:f>`);
     }
 };
